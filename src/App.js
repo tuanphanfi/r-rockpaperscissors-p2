d158dec94b18090c356b4756a8f198ea3ab7273a
tuanphanfi <tuanphan.fi@gmail.com>
2020-07-22 10:58:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
+import ChoiceCard from './components/ChoiceCard.jsx'
 import './App.css';
+import { useState } from "react";
+
+import ChoiceButtons from "./components/ChoiceButtons";
+
+
+
+import { CHOICES, getRoundOutcome } from "./utils";
+
 
 function App() {
-  return (
+  const [prompt, setGamePrompt] = useState("Start");
+
+  const onPlayerChoose = playerChoice => {
+    const [result, compChoice] = getRoundOutcome(playerChoice);
+    console.log(result)
+    console.log(compChoice)
+
+    const newUserChoice = CHOICES[playerChoice];
+    const newComputerChoice = CHOICES[compChoice];
+
+    setPlayerChoice(newUserChoice);
+    setComputerChoice(newComputerChoice);
+
+    if (result === "Victory!") {
+      setPreviousWinner("You");
+    } else if (result === "Defeat!") {
+      setPreviousWinner("Computer");
+    } else {
+      setPreviousWinner("Tie");
+    }
+    setGamePrompt(result);
+    gameHistory.push(result);
+    setGameHistory(gameHistory);
+  };
+  const [gameHistory, setGameHistory] = useState([]);
+
+
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [previousWinner, setPreviousWinner] = useState(null);
+
+  return <div className="App">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row mb-3">
+          <div className="col-md-8 themed-grid-col">
+            <ChoiceCard
+              title="Computer"
+              previousWinner={previousWinner}
+              imgURL={computerChoice && computerChoice.url}
+            />
+            <h1>{prompt}</h1>
+            <ChoiceButtons onPlayerChoose={onPlayerChoose} />
+            <ChoiceCard
+              title="You"
+              previousWinner={previousWinner}
+              imgURL={playerChoice && playerChoice.url}
+            />
+          </div>
+          <div className="col-md-4 themed-grid-col">
+            <h3>History</h3>
+            <ul>
+              {gameHistory.map(result => {
+                return <li>{result}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  </div>;
 }
 
 export default App;
---
+++ src/components/ChoiceCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+const DEFAULT_IMG =
+"http://www.thewateringhole.co.uk/wp-content/uploads/2012/12/play.png";
+
+export default function ChoiceCard(props) {
+    const won = props.title === props.previousWinner;
+    let className;
+    const hasPreviousGame =
+      props.previousWinner === "Computer" || props.previousWinner === "You";
+    if (hasPreviousGame) {
+      className = won ? "winner" : "loser";
+    }
+  
+    let prompt;
+    if (won) {
+      prompt = "Won";
+      className = won ? "winner" : "loser";
+    } else if (props.previousWinner === "Tie") {
+      prompt = "Tie";
+    } else if (props.previousWinner === null) {
+      prompt = "Start";
+    } else {
+      prompt = "Lose";
+    }
+  
+    return (
+      <div className={`choice-card ${className}`}>
+        <h1>{props.title}</h1>
+        <img src={props.imgURL || DEFAULT_IMG} alt={props.title} />
+        <h3>{prompt}</h3>
+      </div>
+    );
+  }
